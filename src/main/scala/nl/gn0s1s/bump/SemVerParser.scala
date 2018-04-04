d6c5d485e40e2a998ebad56b24b4241dcfed1578
Philippus Baalman <philippus@gmail.com>
2018-04-04 22:11:01 +0200

    Refactor parsers to increase readability

--- src/main/scala/nl/gn0s1s/bump/SemVerParser.scala
+++ src/main/scala/nl/gn0s1s/bump/SemVerParser.scala
@@ -3,29 +3,30 @@ package nl.gn0s1s.bump
 import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
 
-sealed trait Separator
-case object DOT extends Separator
-case object HYPHEN extends Separator
-case object PLUS extends Separator
-
 object SemVerParser extends RegexParsers {
-  val numberRegex: Regex = """(0|[1-9]\d*)""".r
-
   val preReleaseRegex: Regex = """((((\d*[A-Za-z-][\dA-Za-z-]*)+)|0|[1-9]\d*)\.)*(((\d*[A-Za-z-][\dA-Za-z-]*)+)|0|[1-9]\d*)""".r
 
   val buildMetadataRegex: Regex = """(([\dA-Za-z-])+\.)*([\dA-Za-z-])+""".r
 
-  private def dot = "." ^^ (_ => DOT)
-  private def hyphen = "-" ^^ (_ => HYPHEN)
-  private def plus = "+" ^^ (_ => PLUS)
-  private def number = numberRegex ^^ { _.toInt }
+  private val number = """0|[1-9]\d*""".r ^^ { _.toInt }
 
-  private def versionPart: Parser[SemVer] = number ~ dot ~ number ~ dot ~ number ^^ {
+  private val dot = "."
+
+  private val versionPart: Parser[SemVer] = number ~ dot ~ number ~ dot ~ number ^^ {
     case major ~ _ ~ minor ~ _ ~ patch =>
       SemVer(major, minor, patch)
   }
-  private def preReleasePart: Parser[String] = hyphen ~ preReleaseRegex ^^ { case _ ~ preRelease => preRelease.toString }
-  private def buildMetadataPart: Parser[String] = plus ~ buildMetadataRegex ^^ { case _ ~ buildMetadata => buildMetadata.toString }
+
+  private val hyphen = "-"
+  private val atLeastOneNonDigit: Regex = """(\d*[A-Za-z-][\dA-Za-z-]*)+""".r
+  private val preReleaseIdentifier = (atLeastOneNonDigit | number) ^^ { _.toString }
+  private val preReleaseParser = repsep(preReleaseIdentifier, dot) ^^ { _.mkString(dot) }
+  private val preReleasePart = hyphen ~ preReleaseParser ^^ { case _ ~ preRelease => preRelease }
+
+  private val plus = "+"
+  private val buildMetadataIdentifier = """([\dA-Za-z-])+""".r ^^ { _.toString }
+  private val buildMetadataParser = repsep(buildMetadataIdentifier, dot) ^^ { _.mkString(dot) }
+  private val buildMetadataPart = plus ~ buildMetadataParser ^^ { case _ ~ buildMetadata => buildMetadata }
 
   def parse(input: String): ParseResult[SemVer] = {
     val parser: Parser[SemVer] =
